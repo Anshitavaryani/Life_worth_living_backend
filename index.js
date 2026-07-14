affe9380b04ba9ba37a4ccd8817e06cdc93f7ccd
Mohit Bhagat <mbhagat1999@gmail.com>
2026-07-14 13:28:35 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-const app = require('./app.js');
-const moment = require('moment');
-const config = require('./src/config/config.js');
-const logger = require('./src/config/logger.js');
+const http = require("http");
+const moment = require("moment");
+
+const app = require("./app.js");
+const config = require("./src/config/config.js");
+const logger = require("./src/config/logger.js");
+
 process.env.TZ = config.DEFAULT_TIMEZONE;
 
-// For Http Server
-// const http = require('http');
-// let servers = http.createServer(app);
+const server = http.createServer(app);
 
-
-// For Https Server
-let https = require('https');
-let fs = require('fs');
-let options = {
-  key: fs.readFileSync('/etc/ssl/virtualmin/17690882161840194/ssl.key'),
-  cert: fs.readFileSync('/etc/ssl/virtualmin/17690882161840194/ssl.cert'),
-  ca: fs.readFileSync('/etc/ssl/virtualmin/17690882161840194/ssl.ca'),
-};
-let servers = https.createServer(options, app);
-
-// Get the current date and time
 const currentTime = moment();
-let server = servers.listen(config.port, () => {
-    logger.info(`Server is Working Fine😊 & Listening to PORT: ${config.port} | Default Timezone is: ${process.env.TZ} | Current date and time: ${currentTime.format('YYYY-MM-DD HH:mm:ss')}`);
+
+server.listen(config.port, () => {
+  logger.info(
+    `Server is Working Fine 😊 & Listening to PORT: ${
+      config.port
+    } | Default Timezone: ${
+      process.env.TZ
+    } | Current date and time: ${currentTime.format("YYYY-MM-DD HH:mm:ss")}`
+  );
 });
 
-
-//server exit operations
+// Server exit operations
 const exitHandler = () => {
-    if (server) {
-        server.close(() => {
-            logger.info('Server closed');
-            process.exit(1);
-        });
-    } else {
-        process.exit(1);
-    }
+  if (server) {
+    server.close(() => {
+      logger.info("Server closed");
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
 };
 
-//unexpectedError handler
 const unexpectedErrorHandler = (error) => {
-    logger.error(error);
-    exitHandler();
+  logger.error(error);
+  exitHandler();
 };
 
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
-process.on('SIGTERM', exitHandler);
-
-
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+process.on("SIGTERM", exitHandler);
